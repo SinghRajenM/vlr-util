f0d74d88b1b42b3146efc290f85d03baa093a954
Rajendra Singh <14791461+SinghRajenM@users.noreply.github.com>
2026-09-10 10:35:04 +0530

    Drop gsl-lite as it is dead code (#11)
    Co-authored-by: Rajendra Singh <rajendra.singh@zscaler.com>

--- vlr-util/cpp_namespace.h
+++ vlr-util/cpp_namespace.h
@@ -5,14 +5,7 @@
 #include <optional>
 #include <memory>
 #include <functional>
-
-#if __has_include(<span>)
-#define VLR_HAS_INCLUDE_STD_SPAN 1
 #include <span>
-#else
-#define VLR_HAS_INCLUDE_STD_SPAN 0
-#include <gsl-lite/gsl-lite.hpp>
-#endif
 
 #include "util.std_aliases.h"
 #include "zstring_view.h"
@@ -30,13 +23,7 @@ using vlr::tzstring_view;
 using std::shared_ptr;
 using std::weak_ptr;
 using std::make_shared;
-
 using std::function;
-
-#if VLR_HAS_INCLUDE_STD_SPAN
 using std::span;
-#else
-using gsl_lite::span;
-#endif
 
 } // namespace cpp
